77d690737876bc2019f04ffacf5c8c699ba9a1cb
Ansh Rastogi <159460435+Anshrastogi05@users.noreply.github.com>
2026-01-21 02:36:53 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/common/header";
 
 const outfit = Outfit({
   // variable: "--font-outfit",
@@ -29,16 +30,7 @@ export default function RootLayout({
         <body
           className={`${outfit.className} antialiased`}
         >
-          <header>
-            <nav className="flex items-center bg-pink-300 justify-between p-4">
-              <div className="text-xl font-bold">Projectory</div>
-              <div className="flex space-x-4">
-                <a href="/about" className="hover:underline">About</a>
-                <a href="/projects" className="hover:underline">Projects</a>
-                <a href="/contact" className="hover:underline">Contact</a>
-              </div>
-            </nav>
-          </header>
+         <Header/>
           {children}
           <footer className="p-4 text-center text-sm text-gray-500">
             &copy; {new Date().getFullYear()} Projectory. All rights reserved.
