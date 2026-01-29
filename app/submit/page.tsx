27d43a38bd4174275a4bc5034fa9892baff5a323
Ansh Rastogi <159460435+Anshrastogi05@users.noreply.github.com>
2026-01-30 02:56:58 +0530

    submit box made

--- app/submit/page.tsx
+++ app/submit/page.tsx
@@ -3,12 +3,13 @@ import SectionHeader from "@/components/common/section-header";
 import { SparkleIcon } from "lucide-react";
 
 export default function SubmitPage() {
-    return (
-        <div className="wrapper">
-         <SectionHeader
-         title: "Submit Your project",
-         icon= {SparkleIcon}
-         
-        </div>
-    );
+  return (
+    <div className="wrapper">
+      <SectionHeader
+        title="Submit Your project"
+        Icon={SparkleIcon}
+        description="Share your creation with the community. Your submission will be reviewed before it goes live."
+      />
+    </div>
+  );
 }
